1db64dcfee9e07c93bc5e9f1cee17cec4d4be346
antonielandrade <antonielandrade@gmail.com>
2026-02-28 18:25:30 -0300

    commit3_280226

---
+++ poo_classes.py
@@ -0,0 +1,35 @@
+
+
+class Casa:
+    def __init__(self, cor, quartos, banheiros):
+        self.cor = cor
+        self.quartos = quartos
+        self.banheiros = banheiros
+      
+    def mostrar_cor(self):
+        print(f'A cor da casa é {self.cor}')
+    def mostrar_quartos(self):
+        print(f'Esta casa tem {self.quartos} quartos')
+    def mostrar_banheiros(self):
+        print(f'Esta casa tem {self.banheiros} banheiros')
+    def adicionar_quarto(self):
+        self.quartos +=1
+        print (f'Agora esta casa tem {self.quartos} quartos')
+    def pintar_casa(self, nova_cor):
+        print(f'Pintando a casa de {self.cor} para {nova_cor}')
+
+
+casa1 = Casa('Azul', 4, 2)
+print('\nCasa 1')
+casa1.mostrar_cor()
+casa1.mostrar_quartos()
+casa1.mostrar_banheiros()
+casa1.adicionar_quarto()
+casa1.pintar_casa('Vermelho')
+
+casa2 = Casa ('Amarela', 6, 3)
+print('\nCasa2')
+casa2.mostrar_cor()
+casa2.mostrar_quartos()
+casa2.mostrar_banheiros()
+casa2.adicionar_quarto()
