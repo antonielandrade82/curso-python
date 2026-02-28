1ae5c3edc279578a83902b90f43dc595014661d7
antonielandrade <antonielandrade@gmail.com>
2026-02-28 12:10:59 -0300

    commit1_280226

---
+++ listas_tuplas_dicionarios.py
@@ -0,0 +1,54 @@
+#LISTAS ( são mutáveis e sintaxe com colchetes)
+
+# frutas = ['Abacate', 'Banana', 'Laranja', 'Melancia']
+
+# frutas.append('Uva')
+# frutas.append('Laranja')
+# frutas.remove('Banana')
+
+# print(len(frutas))
+# print(frutas)
+
+# tarefas = [  
+# ]
+# tarefas.append('Estudar Python com IA ')
+# tarefas.append('Fazer exercícios de Python ')
+# tarefas.append('Ler um livro sobre Python ')
+
+# print('Minhas tarefas diárias:')
+# print(tarefas[0])
+# print(tarefas[1])
+# print(tarefas[2])
+
+# print('Minhas tarefas diárias:')
+# for tarefa in tarefas:
+#     print(f'Tarefa: {tarefa}')
+
+
+    #TUPLAS (são imutáveis e sintaxe com parênteses)
+
+# meses = ('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro')
+# print(meses[0])  # Acessando o primeiro mês
+
+# DICIONÁRIOS (são mutáveis e sintaxe com chaves)
+
+# usuario = {
+#     'nome': 'André',
+#     'idade': 30,
+#     'Departamento': 'TI'
+# }
+
+# usuario['idade'] = 43
+# usuario['Cidade'] = 'São Paulo'
+# # print(usuario['nome'])  # Acessando o nome do usuário
+# # print(usuario['idade'])  # Acessando a idade do usuário
+
+# print(usuario)
+
+aluno = {
+    'nome': input('Digite o nome do aluno: '),
+    'idade': int(input('Digite a idade do aluno: ')),
+    'nota': float(input('Digite a nota do aluno: '))
+}
+
+print(f'{aluno["nome"]} tem {aluno["idade"]} anos e sua nota é {aluno["nota"]}.')
